3d839613a84fcabc0ec39cfe091fa0be92bc6e3c
yoomin-0599 <whfusjtjdjwjf@gmail.com>
2025-08-28 15:01:45 +0900

    Update vite.config.ts

--- frontend/news-app/vite.config.ts
+++ frontend/news-app/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/streamlit_04/',
+  //base: '/streamlit_04/',
   server: {
     port: 5173,
     proxy: {
